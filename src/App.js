dab81f1d1cea4959c69ec4441ab688d76ac19d8e
mohammadnedaei <nedaei2002@gmail.com>
2022-08-02 13:38:55 +0430

    removed todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// TODO: Three pages with smooth linking fade - one for info for users telling them this page is not ready so we're
-// redirecting you to counter page - the counter page - and the last page will be the celebration and introdutions
 import AppRoutes from "./routes";
 const App = () => {
     return (
